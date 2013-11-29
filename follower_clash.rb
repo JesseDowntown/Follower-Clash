449ad0b9d6fe1576d548fcd09aa885e67be25d5a
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-11-29 10:51:06 -0600

    Refactor UserComparer::User to set @client variable at initialize. Add friends method to compare friends. Refactor UserCompararer::Compare#compare to handle multiple types of comparison.

--- follower_clash.rb
+++ follower_clash.rb
@@ -3,3 +3,15 @@ require 'bundler/setup'
 
 require_relative 'lib/user_comparer'
 
+require_relative 'lib/user_comparer'
+
+user1 = UserComparer::User.new('jkasnicki')
+user2 = UserComparer::User.new('jessedowntown')
+compareType = 'friends'
+puts "#{user1.username}'s #{compareType} count: #{user1.send(compareType)}"
+puts "#{user2.username}'s #{compareType} count: #{user2.send(compareType)}"
+comparer = UserComparer::Comparer.new(user1, user2)
+
+user_with_most_followers = comparer.compare(compareType)
+
+puts "And the user with the most followers is: #{user_with_most_followers}"
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -8,17 +8,20 @@ module UserComparer
 		attr_reader :username
 		def initialize (username)
 			@username = username
-		end
-
-		def followers
-			client = Twitter::REST::Client.new do |config|
+			@client = Twitter::REST::Client.new do |config|
 				config.consumer_key        = ENV['CONSUMER_KEY']
 				config.consumer_secret     = ENV['CONSUMER_SECRET']
 				config.access_token        = ENV['OAUTH_TOKEN']
 				config.access_token_secret = ENV['OAUTH_TOKEN_SECRET']
 			end
+		end
 
-			 client.user(@username)followers_count
+		def followers
+			 @client.user(@username).followers_count
+		end
+
+		def friends
+			 @client.user(@username).friends_count
 		end
 	end
 
@@ -26,6 +29,16 @@ module UserComparer
 		def initialize (user1, user2)
 			@user1 = user1
 			@user2 = user2
+		end
+
+		def compare(compareType)
+			if (@user1.send(compareType) > @user2.send(compareType))
+				return @user1.username
+			elsif (@user1.send(compareType) < @user2.send(compareType))
+				return @user2.username
+			else
+				return "equal number of followers"
+			end
 		end
 	end
 end
